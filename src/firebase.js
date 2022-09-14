b8cf60383da360a700c35212187c5da300f1f5a4
kevinalbino <kevinalbi4625@gmail.com>
2022-09-14 18:52:07 -0400

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD-gFtnelWlbURbwtZ-tRkNY5cKqJvPO_U",
+    authDomain: "netflix-clone-b9627.firebaseapp.com",
+    projectId: "netflix-clone-b9627",
+    storageBucket: "netflix-clone-b9627.appspot.com",
+    messagingSenderId: "169528237506",
+    appId: "1:169528237506:web:d89dc267ae226255de8a5b"
+};
