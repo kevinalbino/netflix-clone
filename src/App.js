1f023e5138d84f8bed0df740d25ba304006b1251
kevinalbino <kevinalbi4625@gmail.com>
2022-09-15 18:57:32 -0400

    Router setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomeScreen from './pages/HomeScreen';
+import LoginScreen from './pages/LoginScreen';
 
 function App() {
+  const user = null;
+
   return (
     <div className="app">
-      <HomeScreen />
+      <Router>
+        {!user ? (
+          <LoginScreen />
+        ) : (
+          <Routes>
+            <Route exact path="/" element={<HomeScreen />}>
+            </Route>
+          </Routes>
+        )}
+      </Router>
     </div>
   );
 }
