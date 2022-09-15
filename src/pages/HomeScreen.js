40cc1807f490f4cf3f86664ab6567eda3a1dd4bf
kevinalbino <kevinalbi4625@gmail.com>
2022-09-15 11:43:23 -0400

    included Row components on HomeScreen

--- src/pages/HomeScreen.js
+++ src/pages/HomeScreen.js
@@ -2,12 +2,26 @@ import React from 'react';
 import './HomeScreen.css';
 import Nav from '../components/Nav';
 import Banner from '../components/Banner';
+import requests from '../Requests';
+import Row from '../components/Row';
 
 function HomeScreen() {
     return (
         <div className="HomeScreen">
             <Nav />
             <Banner />
+            <Row
+                title="NETFLIX ORIGINALS"
+                fetchURL={requests.fetchNetflixOriginals}
+                isLargeRow
+            />
+            <Row title="Trending Now" fetchURL={requests.fetchTrending} />
+            <Row title="Top Rated" fetchURL={requests.fetchTopRated} />
+            <Row title="Action Movies" fetchURL={requests.fetchActionMovies} />
+            <Row title="Comedy Movies" fetchURL={requests.fetchComedyMovies} />
+            <Row title="Horror Movies" fetchURL={requests.fetchHorrorMovies} />
+            <Row title="Romance Movies" fetchURL={requests.fetchRomanceMovies} />
+            <Row title="Documentaries" fetchURL={requests.fetchDocumentaries} />
         </div>
     );
 }
