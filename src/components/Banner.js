e93ef5cebda32b7270e0b6cd01eb916d122667ab
kevinalbino <kevinalbi4625@gmail.com>
2022-09-14 21:30:43 -0400

    finished nav component and some of banner

---
+++ src/components/Banner.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import './Banner.css';
+
+function Banner() {
+
+    function truncate(string, n) {
+        return string?.length > n ? string.substr(0, n - 1) + '...' : string;
+    }
+
+    return (
+        <header
+            className="banner"
+            style={{
+                backgroundSize: "cover",
+                backgroundImage: `url("https://upload.wikimedia.org/wikipedia/commons/6/68/Solid_black.png")`,
+                backgroundPosition: "center center",
+            }}
+        >
+            <div className="banner__contents">
+                <h1 className="banner__title">Movie Name</h1>
+                <div className="banner__buttons">
+                    <button className="banner__button">Play</button>
+                    <button className="banner__button">My List</button>
+                </div>
+                <h1 className="banner__description">
+                    {truncate(`Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                    nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+                    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                    deserunt mollit anim id est laborum.`, 150)}
+                </h1>
+            </div>
+            <div className="banner--fadeBottom" />
+        </header>
+    );
+}
+
+export default Banner;
