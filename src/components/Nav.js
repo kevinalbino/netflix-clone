a9abe9c481bb7d614676a1af4a1014af62031a03
kevinalbino <kevinalbi4625@gmail.com>
2022-09-16 22:41:13 -0400

    implemented router navigation for other page links

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import './Nav.css';
+import { useNavigate } from 'react-router-dom';
 
 function Nav() {
     const [show, handleShow] = useState(false);
+    const navigate = useNavigate();
+
     const transitionNavBar = () => {
         if (window.scrollY > 100) {
             handleShow(true);
@@ -20,11 +23,13 @@ function Nav() {
         <div className={`nav ${show && 'nav--bg'}`}>
             <div className="nav_contents">
                 <img
+                    onClick={() => navigate("/")}
                     className="nav__logo"
                     src="https://upload.wikimedia.org/wikipedia/commons/7/7a/Logonetflix.png"
                     alt=""
                 />
                 <img
+                    onClick={() => navigate("/profile")}
                     className="nav__avatar"
                     src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
                     alt=""
